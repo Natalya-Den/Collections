3aee84f74fb10c8ba41d3067e508d6e0a60b86c0
Natalya Denisenko <ionova.natalya@mail.ru>
2025-01-20 22:58:14 +0300

    Implemented HashMap

--- src/main/java/ru/netology/collections/Game.java
+++ src/main/java/ru/netology/collections/Game.java
@@ -1,36 +1,25 @@
 package ru.netology.collections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Game {
 
-    List<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
+    int player1Strength = 0;
+    int player2Strength = 0;
 
     public void register(Player player) {
-        players.add(player);
-    }
-
-    public Player findByName(String name) {
-        for (Player player : players) {
-            if (player.getName().equals(name)) {
-                return player;
-            }
-        }
-        return null;
+        players.put(player.getName(), player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
-
-        if (player1 == null) {
+        if (!players.containsKey(playerName1)) {
             throw new NotRegisteredException(playerName1);
-        } else if (player2 == null) {
+        } else if (!players.containsKey(playerName2)) {
             throw new NotRegisteredException(playerName2);
-        } else if (player1.getStrength() > player2.getStrength()) {
+        } else if (players.get(playerName1) > players.get(playerName2)) {
             return 1;
-        } else if (player1.getStrength() < player2.getStrength()) {
+        } else if (players.get(playerName1) < players.get(playerName2)) {
             return 2;
         } else {
             return 0;
